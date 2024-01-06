447cf0804ee1ee83fab953f894ea4d3098ade396
jj-lewis <lewis123jam@gmail.com>
2024-01-06 20:40:51 +0200

    wip

--- src/args.rs
+++ src/args.rs
@@ -6,8 +6,38 @@ use clap::{
 
 /// Program to assist with Kuroro Beast type battling
 #[derive(Debug, Parser)]
-#[clap(author, version, about)]
+#[command(name = "MyApp")]
+#[command(author = "Kevin K. <kbknapp@gmail.com>")]
+#[command(version = "1.0")]
+#[command(about = "Does awesome things", long_about = None)]
 pub struct KuroroArgs {
-    /// FIRST ARGUMENT
-    pub first_argument: String
+    /// Flag (-e, -b)
+    #[clap(subcommand)]
+    pub subcommand: Flag
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Flag {
+    /// All Element related information
+    Element(ElementCommand),
+}
+
+#[derive(Debug, Args)]
+pub struct ElementCommand {
+    #[clap(subcommand)]
+    pub command: ElementSubCommand
+}
+
+#[derive(Debug, Subcommand)]
+pub enum ElementSubCommand {
+    /// Show specific element
+    Show(ShowElement),
+    /// List all elements
+    List
+}
+
+#[derive(Debug, Args)]
+pub struct ShowElement {
+    // name of the element
+    pub name: String
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,19 @@ mod elements;
 mod beasts;
 mod args;
 
-use args::KuroroArgs;
+use args::{KuroroArgs, Flag, ShowElement};
 use clap::Parser;
 fn main() {
     // command line arguments -> -e Fire outputs Fire element struct
     // -b Name outputs beast struct
     let args = KuroroArgs::parse();
+    // only hits here once satisfying arg structure
+    match &args.subcommand {
+        Flag::Element (_i) => {
+            match &_i.command {
+                args::ElementSubCommand::Show (_c) => { println!( "show command - args: {:?}", _c.name)},
+                args::ElementSubCommand::List => { println!("list command.")},
+            }
+         }
+    }
 }
